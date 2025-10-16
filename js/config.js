afc8336af7a188f25d7dec799af1735e9d2235c0
huchunyi <45680337+huchunyi@users.noreply.github.com>
2025-10-16 20:04:41 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -14,7 +14,7 @@ const PASSWORD_CONFIG = {
 // 网站信息配置
 const SITE_CONFIG = {
     name: 'LibreTV',
-    url: 'https://tv.vs98k.cn/',
+    url: 'https://libretv.is-an.org',
     description: '免费在线视频搜索与观看平台',
     logo: 'image/logo.png',
     version: '1.0.3'
